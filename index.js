80db3f42370f462495f18bc6214a8cb945a89896
Sara Webb <saracharlotterose@gmail.com>
2020-07-04 18:03:39 -0700

    saving changes

--- index.js
+++ index.js
@@ -1,122 +1,97 @@
 // const inquirer = require("inquirer")
 
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Choices = require("inquirer/lib/objects/choices");
+main();
 
-main()
+function main() {
+  userinput().then((answers) => {
+    const html = renderreadme(answers);
+    console.log(html);
+    writeFile(html)
+  });
 
-function main(){
+  // inquirer
 
-    userinput()
-    .then(answers =>  {
-        const html = renderreadme(answers);
-        console.log(html)
+  // userinput().then(answers => console.log(answers));
 
+  function userinput() {
+    const questions = [
+      {
+        name: "title",
+        type: "input",
+        message: "Please enter the title of your README",
+      },
+      {
+        name: "description",
+        type: "input",
+        message: "Please enter a description for your README",
+      },
+      {
+        name: "contents",
+        type: "input",
+        message: "please enter a table of contents",
+      },
+      {
+        name: "installation",
+        type: "input",
+        message: "Please enter a method of installation",
+      },
+      {
+        name: "usage",
+        type: "input",
+        message: "Please specify usage",
+      },
+      {
+        name: "license",
+        type: "list",
+        message: "Please enter license",
+        choices: ["ISC", "MIT"],
+      },
+      {
+        name: "contributing",
+        type: "input",
+        message: "Please enter contributing members",
+      },
+      {
+        name: "tests",
+        type: "input",
+        message: "Please enter tests",
+      },
+      {
+        name: "questions",
+        type: "input",
+        message: "Please enter questions",
+      },
+      {
+        name: "githuburl",
+        type: "input",
+        message: "Please enter github username",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Please enter email",
+      },
+    ];
+    return inquirer.prompt(questions);
+  }
 
-
-});
-
-// inquirer
-
-userinput().then(answers => console.log(answers));
-
-function userinput(){
-
-const questions =[
-    {
-        name:"title",
-        type:"input",
-        message:"Please enter the title of your README"
-    },
-    {
-        name:"description",
-        type:"input",
-        message:"Please enter a description for your README"
-    },
-    {
-        name:"contents",
-        type:"input",
-        message:"please enter a table of contents"
-    },
-    {
-        name:"installation",
-        type:"input",
-        message:"Please enter a method of installation"
-
-    },
-    {
-        name:"usage",
-        type:"input",
-        message:"Please specify usage"
-
-    },
-    {
-        name:"license",
-        type:"input",
-        message:"Please enter license"
-
-    },
-    {
-        name:"contributing",
-        type:"input",
-        message:"Please enter contributing members"
-
-    },
-    {
-        name:"tests",
-        type:"input",
-        message:"Please enter tests"
-
-    },
-    {
-        name:"questions",
-        type:"input",
-        message:"Please enter questions"
-
-    },
-    {
-        name:"githuburl",
-        type:"input",
-        message:"Please enter github username"
-
-    },
-    {
-        name:"email",
-        type:"input",
-        message:"Please enter email"
-
-    }
-
-
-
-];
- return inquirer.prompt(questions)
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function renderreadme({title, description, contents, installation, usage,liscense,contributing,tests,questions}){
-return `## **${title}**
+  function renderreadme({
+    title,
+    description,
+    contents,
+    installation,
+    usage,
+    license,
+    contributing,
+    tests,
+    questions,
+    githuburl,
+    email,
+  }) {
+    return `## **${title}**
 
 
 ## **Description**
@@ -134,8 +109,8 @@ ${installation}
 ${usage}
 
 
-## **Liscense**
-${liscense}
+## **License**
+${license}
 ## **Contributing**
 ${contributing}
 ## **Tests**
@@ -143,6 +118,15 @@ ${tests}
 
 ## **Questions**
 ${questions}
+${githuburl}
+${email}
 
 `;
-}}
+  }
+}
+
+
+function writeFile(data) {
+  fs.writeFileSync("./test.md", data);
+  console.log("after the file write");
+}
---
+++ test.md
@@ -0,0 +1,30 @@
+## **s**
+
+
+## **Description**
+s
+
+## **Table** **of** **contents**
+s
+
+
+## **Installation**
+s
+
+
+## **Usage**
+s
+
+
+## **License**
+ISC
+## **Contributing**
+s
+## **Tests**
+s
+
+## **Questions**
+s
+s
+s
+
